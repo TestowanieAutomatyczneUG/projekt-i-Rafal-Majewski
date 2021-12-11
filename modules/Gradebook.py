358b7f3d9258620cfc2c7a9990b20b2f4c48630a
Rafał Majewski <goodheropl@gmail.com>
2021-12-11 20:21:25 +0100

    gradebook: school name setter and getter

--- modules/Gradebook.py
+++ modules/Gradebook.py
@@ -1,7 +1,15 @@
 class Gradebook:
 	def __init__(self, *, schoolName: str) -> None:
+		self.schoolName = schoolName
+
+	@property
+	def schoolName(self) -> str:
+		return self.__schoolName
+
+	@schoolName.setter
+	def schoolName(self, schoolName: str) -> None:
 		if not isinstance(schoolName, str):
 			raise TypeError("School name must be a string")
 		if not schoolName:
 			raise ValueError("School name cannot be empty")
-		self.schoolName = schoolName
+		self.__schoolName = schoolName
