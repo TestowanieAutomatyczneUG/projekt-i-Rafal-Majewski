ebe6aec52cbb62ec0661c91e0d8b9564150a1630
Rafał Majewski <goodheropl@gmail.com>
2021-12-11 20:47:02 +0100

    gradebook tests: rename class

--- tests/Test_Gradebook.py
+++ tests/Test_Gradebook.py
@@ -2,18 +2,19 @@ import unittest
 from modules.Gradebook import Gradebook
 
 
-class Test_Gradebook(unittest.TestCase):
-	def test_constructor_no_arguments(self):
+class Test_Gradebook_constructor(unittest.TestCase):
+	def test_no_arguments(self):
 		self.assertRaises(TypeError, Gradebook, [])
 
-	def test_constructor_correct_name(self):
+	def test_correct_name(self):
 		gradebook = Gradebook(schoolName="Test")
 		self.assertEqual(gradebook.schoolName, "Test")
 
-	def test_constructor_incorrect_name(self):
+	def test_incorrect_name(self):
 		with self.assertRaises(ValueError):
 			Gradebook(schoolName="")
 
-	def test_constructor_name_of_wrong_type(self):
+	def test_name_of_wrong_type(self):
 		with self.assertRaises(TypeError):
 			Gradebook(schoolName=53)
+
