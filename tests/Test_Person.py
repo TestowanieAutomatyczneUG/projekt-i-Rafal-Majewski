414662ce1d99399b602d2940a38776b917b7fc3b
Rafał Majewski <goodheropl@gmail.com>
2021-12-12 12:13:47 +0100

    test person: constructor with missing lastname or firstname

--- tests/Test_Person.py
+++ tests/Test_Person.py
@@ -5,3 +5,11 @@ from modules.Person import Person
 class Test_Person_constructor(unittest.TestCase):
 	def test_no_arguments(self):
 		self.assertRaises(TypeError, Person, [])
+
+	def test_missing_lastName(self):
+		with self.assertRaises(TypeError):
+			Person(firstName="Jan")
+
+	def test_missing_firstName(self):
+		with self.assertRaises(TypeError):
+			Person(lastName="Kowalski")
