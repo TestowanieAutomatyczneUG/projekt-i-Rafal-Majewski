d0fc4dfc0137176d6612bad117615d1586801931
Rafał Majewski <goodheropl@gmail.com>
2021-12-12 12:07:31 +0100

    person: add firstname and last name setters and getters

--- modules/Person.py
+++ modules/Person.py
@@ -2,3 +2,19 @@ class Person:
 	def __init__(self, *, firstName: str, lastName: str) -> None:
 		self.firstName = firstName
 		self.lastName = lastName
+
+	@property
+	def firstName(self) -> str:
+		return self.__firstName
+
+	@firstName.setter
+	def firstName(self, firstName: str) -> None:
+		self.__firstName = firstName
+
+	@property
+	def lastName(self) -> str:
+		return self.__lastName
+
+	@lastName.setter
+	def lastName(self, lastName: str) -> None:
+		self.__lastName = lastName
