97a67ee969a1cae1d6f5ccdb3f6799cb3efa7e18
Rafał Majewski <goodheropl@gmail.com>
2021-12-16 20:04:55 +0100

    fix gradebook no arguments test

--- tests/Test_Gradebook.py
+++ tests/Test_Gradebook.py
@@ -7,7 +7,7 @@ from modules.Subject import Subject
 
 class Test_Gradebook_constructor(unittest.TestCase):
 	def test_no_arguments(self):
-		self.assertRaises(TypeError, Gradebook, [])
+		self.assertRaises(TypeError, Gradebook)
 
 	def test_correct_name(self):
 		gradebook = Gradebook(schoolName="Test")
