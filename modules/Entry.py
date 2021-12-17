6a8336daa8caeb72a11a2b44db93b40c760bc1aa
Rafał Majewski <goodheropl@gmail.com>
2021-12-17 13:35:51 +0100

    test if entry assigns datetime

--- modules/Entry.py
+++ modules/Entry.py
@@ -3,4 +3,12 @@ from datetime import datetime as Datetime
 
 class Entry:
 	def __init__(self, datetime: Datetime) -> None:
+		self.datetime = datetime
+
+	@property
+	def datetime(self) -> Datetime:
+		return self.__datetime
+
+	@datetime.setter
+	def datetime(self, datetime: Datetime) -> None:
 		self.__datetime = datetime
--- tests/Test_Entry.py
+++ tests/Test_Entry.py
@@ -1,7 +1,13 @@
 import unittest
 from modules.Entry import Entry
+from datetime import datetime as Datetime
 
 
 class Test_Entry_constructor(unittest.TestCase):
 	def test_no_arguments(self):
 		self.assertRaises(TypeError, Entry)
+
+	def test_if_assigns_datetime(self):
+		datetime = Datetime(year=2020, month=1, day=1)
+		entry = Entry(datetime)
+		self.assertEqual(entry.datetime, datetime)
