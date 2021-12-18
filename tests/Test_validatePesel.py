54ff28bbce5a1b387412fe7a08ee10f45d612233
Rafał Majewski <goodheropl@gmail.com>
2021-12-18 03:09:36 +0100

    validatePesel: return false if incorrect checksum

--- tests/Test_validatePesel.py
+++ tests/Test_validatePesel.py
@@ -1,5 +1,6 @@
 import unittest
 from modules.utils import validatePesel
+from parameterized import parameterized  # type: ignore
 
 
 class Test_validatePesel(unittest.TestCase):
@@ -17,3 +18,13 @@ class Test_validatePesel(unittest.TestCase):
 
 	def test_incorrect_day(self):
 		self.assertFalse(validatePesel("97093537961"))
+
+	@parameterized.expand([
+		"77070138891",
+		"05290866323",
+		"63070469217",
+		"86071681772",
+		"00262842997",
+	])
+	def test_incorrect_checksum(self, pesel: str):
+		self.assertFalse(validatePesel(pesel))
