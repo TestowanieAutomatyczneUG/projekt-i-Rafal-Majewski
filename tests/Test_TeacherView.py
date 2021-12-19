c590be5a714d0ec685f46966672814f39db5e229
Rafał Majewski <goodheropl@gmail.com>
2021-12-19 11:54:37 +0100

    TeacherView: test if constructor works for correct arguments

--- tests/Test_TeacherView.py
+++ tests/Test_TeacherView.py
@@ -1,7 +1,14 @@
 import unittest
 from modules.TeacherView import TeacherView
+from modules.Gradebook import Gradebook
+from modules.Teacher import Teacher
 
 
 class Test_TeacherView_constructor(unittest.TestCase):
 	def test_no_arguments(self):
 		self.assertRaises(TypeError, TeacherView)
+
+	def test_correct(self):
+		gradebook = Gradebook(schoolName="Test")
+		teacher = Teacher(firstName="Jan", lastName="Kowalski", pesel="85052342517")
+		TeacherView(gradebook=gradebook, teacher=teacher)
