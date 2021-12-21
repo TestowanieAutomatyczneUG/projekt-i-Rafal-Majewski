1854adcee49746b37d437e9a0942fe9d5ca58fb2
Rafał Majewski <goodheropl@gmail.com>
2021-12-21 21:04:01 +0100

    Subject: test name

--- modules/Subject.py
+++ modules/Subject.py
@@ -1,3 +1,11 @@
 class Subject:
 	def __init__(self, name: str) -> None:
-		pass
+		self.__name = name
+
+	@property
+	def name(self) -> str:
+		return self.__name
+
+	@name.setter
+	def name(self, name: str) -> None:
+		self.__name = name
--- tests/Test_Subject.py
+++ tests/Test_Subject.py
@@ -5,3 +5,7 @@ from modules.Subject import Subject
 class Test_Subject_constructor(unittest.TestCase):
 	def test_no_arguments(self):
 		self.assertRaises(TypeError, Subject)
+
+	def test_name(self):
+		subject = Subject(name="Math")
+		self.assertEqual(subject.name, "Math")
