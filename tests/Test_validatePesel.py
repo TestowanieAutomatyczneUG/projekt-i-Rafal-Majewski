06716b560d356df7bb5ec3e058919720c937d5a7
Rafał Majewski <goodheropl@gmail.com>
2021-12-21 22:33:18 +0100

    validatePesel: test many invalid pesels

--- tests/Test_validatePesel.py
+++ tests/Test_validatePesel.py
@@ -47,3 +47,9 @@ class Test_validatePesel(unittest.TestCase):
 		pesels = load("./tests/data/valid_pesels.txt")
 		for pesel in pesels:
 			assert_that(validatePesel(pesel), is_(True))
+
+	def test_many_invalid_pesels(self):
+		pesels = load("./tests/data/invalid_pesels.txt")
+		for pesel in pesels:
+			print(pesel)
+			assert_that(validatePesel(pesel), is_(False))
