e609aa0f33dda678fd4dac867fc5ae357789c9b6
Rafał Majewski <goodheropl@gmail.com>
2021-12-21 21:33:41 +0100

    Grade: constructor

--- modules/Grade.py
+++ modules/Grade.py
@@ -1,5 +1,19 @@
 from modules.Entry import Entry
+from modules.Teacher import Teacher
+from modules.Subject import Subject
+from datetime import datetime as Datetime
 
 
 class Grade(Entry):
-	pass
+	def __init__(
+		self,
+		*,
+		subject: Subject,
+		teacher: Teacher,
+		value: str,
+		datetime: Datetime
+	) -> None:
+		super().__init__(datetime=datetime)
+		self.__subject = subject
+		self.__teacher = teacher
+		self.__value = value
--- tests/Test_Grade.py
+++ tests/Test_Grade.py
@@ -1,7 +1,22 @@
 import unittest
 from modules.Grade import Grade
+from modules.Subject import Subject
+from modules.Teacher import Teacher
+from modules.GradeValue import GradeValue
+from datetime import datetime as Datetime
 
 
 class Test_Grade_constructor(unittest.TestCase):
 	def test_no_arguments(self):
 		self.assertRaises(TypeError, Grade)
+
+	def test_correct(self):
+		subject = Subject(name="Math")
+		teacher = Teacher(firstName="Jan", lastName="Kowalski", pesel="85052342517")
+		datetime = Datetime(year=2020, month=1, day=1)
+		Grade(
+			subject=subject,
+			teacher=teacher,
+			value=GradeValue.G3PLUS,
+			datetime=datetime
+		)
