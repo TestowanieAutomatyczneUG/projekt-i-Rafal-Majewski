be10e837944179724d49dabab895fa27b1cc5adb
Rafał Majewski <goodheropl@gmail.com>
2021-12-21 21:26:32 +0100

    Teacher: allow constructing with subjects

--- modules/Teacher.py
+++ modules/Teacher.py
@@ -1,5 +1,27 @@
 from modules.Person import Person
+from modules.Subject import Subject
 
 
 class Teacher(Person):
-	pass
+	def __init__(
+		self,
+		*,
+		firstName: str,
+		lastName: str,
+		pesel: str,
+		subjects: set[Subject] = None
+	) -> None:
+		self.__subjects = set[Subject]()
+		if subjects is not None:
+			for subject in subjects:
+				self.addSubject(subject)
+		super().__init__(firstName=firstName, lastName=lastName, pesel=pesel)
+
+
+	@property
+	def subjects(self) -> frozenset[Subject]:
+		return frozenset(self.__subjects)
+
+	def addSubject(self, subject: Subject) -> Subject:
+		self.__subjects.add(subject)
+		return subject
--- tests/Test_Teacher.py
+++ tests/Test_Teacher.py
@@ -1,8 +1,22 @@
 import unittest
 from modules.Teacher import Teacher
+from modules.Subject import Subject
 
 
 class Test_Teacher_constructor(unittest.TestCase):
 	def test_no_arguments(self):
 		with self.assertRaises(TypeError):
 			Teacher()
+
+	def test_with_subjects(self):
+		subjects = set([
+			Subject(name="Math"),
+			Subject(name="Physics"),
+		])
+		teacher = Teacher(
+			firstName="Jan",
+			lastName="Kowalski",
+			pesel="85052342517",
+			subjects=subjects
+		)
+		self.assertEqual(teacher.subjects, subjects)
