69877ba89e85fc911d76afc3a76b9477bac30d30
Rafał Majewski <goodheropl@gmail.com>
2021-12-21 18:51:22 +0100

    Comment: test if content is set

--- modules/Comment.py
+++ modules/Comment.py
@@ -1,5 +1,18 @@
 from modules.Entry import Entry
+from datetime import datetime as Datetime
 
 
 class Comment(Entry):
-	pass
+	def __init__(self, *, datetime: Datetime, content: str) -> None:
+		super().__init__(datetime=datetime)
+		self.__content = content
+
+	@property
+	def content(self) -> str:
+		return self.__content
+
+	@content.setter
+	def content(self, content: str) -> None:
+		if not isinstance(content, str):
+			raise TypeError("Content must be a string.")
+		self.__content = content
