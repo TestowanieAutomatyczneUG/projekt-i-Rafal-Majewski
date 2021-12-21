fbb95bc16b13516d4ed5139527bacc81a0973d1e
Rafał Majewski <goodheropl@gmail.com>
2021-12-21 22:00:54 +0100

    fix function put in wrong class

--- tests/Test_TeacherView.py
+++ tests/Test_TeacherView.py
@@ -92,6 +92,31 @@ class Test_TeacherView_giveGrade(unittest.TestCase):
 		with self.assertRaises(ValueError):
 			teacherView.giveGrade(student, grade)
 
+	def test_if_adds(self):
+		subject = Subject(name="Math")
+		student = Student(
+			firstName="Jan",
+			lastName="Kowalski",
+			pesel="85052342517",
+			subjects=[subject]
+		)
+		teacher = Teacher(
+			firstName="John",
+			lastName="Smith",
+			pesel="96071361238",
+			subjects=[subject]
+		)
+		teacherView = TeacherView(teacher)
+		datetime = Datetime(year=2020, month=1, day=1)
+		grade = Grade(
+			teacher=teacher,
+			subject=subject,
+			datetime=datetime,
+			value=GradeValue.G3PLUS
+		)
+		teacherView.giveGrade(student, grade)
+		self.assertIn(grade, student.grades)
+
 
 class Test_takeGrade(unittest.TestCase):
 	def test_return_value(self):
@@ -118,28 +143,3 @@ class Test_takeGrade(unittest.TestCase):
 		)
 		teacherView.giveGrade(student, grade)
 		self.assertIs(teacherView.takeGrade(student, grade), grade)
-
-	def test_if_adds(self):
-		subject = Subject(name="Math")
-		student = Student(
-			firstName="Jan",
-			lastName="Kowalski",
-			pesel="85052342517",
-			subjects=[subject]
-		)
-		teacher = Teacher(
-			firstName="John",
-			lastName="Smith",
-			pesel="96071361238",
-			subjects=[subject]
-		)
-		teacherView = TeacherView(teacher)
-		datetime = Datetime(year=2020, month=1, day=1)
-		grade = Grade(
-			teacher=teacher,
-			subject=subject,
-			datetime=datetime,
-			value=GradeValue.G3PLUS
-		)
-		teacherView.giveGrade(student, grade)
-		self.assertIn(grade, student.grades)
