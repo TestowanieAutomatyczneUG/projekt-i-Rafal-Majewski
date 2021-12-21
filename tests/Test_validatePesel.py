452ee4530cc6d02e51d23b5853fff90878d67e17
Rafał Majewski <goodheropl@gmail.com>
2021-12-21 19:44:53 +0100

    fix Test_validatePesel
    Test_validatePesel checked a custom matcher instead of a real function

--- tests/Test_validatePesel.py
+++ tests/Test_validatePesel.py
@@ -1,24 +1,21 @@
 import unittest
 from parameterized import parameterized  # type: ignore
-from hamcrest import assert_that, not_
-from ValidPesel import valid_pesel
+from hamcrest import assert_that, is_
+from modules.utils import validatePesel
 
 
 class Test_validatePesel(unittest.TestCase):
-	def test_wrong_length(self):
-		assert_that("12345678901", not_(valid_pesel()))
-
 	def test_correct(self):
-		assert_that("97092537961", valid_pesel())
+		assert_that(validatePesel("97092537961"), is_(True))
 
 	def test_non_digit(self):
-		assert_that("9709a537961", not_(valid_pesel()))
+		assert_that(validatePesel("9709a537961"), is_(False))
 
 	def test_incorrect_month(self):
-		assert_that("97142537969", not_(valid_pesel()))
+		assert_that(validatePesel("97142537969"), is_(False))
 
 	def test_incorrect_day(self):
-		assert_that("97093537961", not_(valid_pesel()))
+		assert_that(validatePesel("97093537961"), is_(False))
 
 	@parameterized.expand([
 		"77070138891",
@@ -28,10 +25,10 @@ class Test_validatePesel(unittest.TestCase):
 		"00262842997",
 	])
 	def test_incorrect_checksum(self, pesel: str):
-		assert_that(pesel, not_(valid_pesel()))
+		assert_that(validatePesel(pesel), is_(False))
 
 	def test_too_short(self):
-		assert_that("123456789", not_(valid_pesel()))
+		assert_that(validatePesel("123456789"), is_(False))
 
 	def test_too_long(self):
-		assert_that("1234567890123", not_(valid_pesel()))
+		assert_that(validatePesel("1234567890123"), is_(False))
