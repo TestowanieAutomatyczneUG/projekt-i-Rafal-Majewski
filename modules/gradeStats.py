dad0ed452658dc7f1a3d01460e2635a14c58cbca
Rafał Majewski <goodheropl@gmail.com>
2021-12-23 00:27:19 +0100

    gradeStats: rename totalAverage function to calculateTotalAverage

--- modules/gradeStats.py
+++ modules/gradeStats.py
@@ -2,7 +2,7 @@ from typing import Optional
 from modules.Grade import Grade
 
 
-def totalAverage(grades: set[Grade]) -> Optional[float]:
+def calculateTotalAverage(grades: set[Grade]) -> Optional[float]:
 	if len(grades) == 0:
 		return None
 	return sum(grade.value.value for grade in grades) / len(grades)
--- tests/Test_gradeStats.py
+++ tests/Test_gradeStats.py
@@ -1,4 +1,4 @@
-from modules.gradeStats import totalAverage
+from modules.gradeStats import calculateTotalAverage
 from assertpy import assert_that  # type: ignore
 from modules.Grade import Grade
 from modules.Teacher import Teacher
@@ -9,7 +9,7 @@ from modules.GradeValue import GradeValue
 
 class Test_totalAverage:
 	def test_empty(self):
-		assert_that(totalAverage(set[Grade]())).is_none()
+		assert_that(calculateTotalAverage(set[Grade]())).is_none()
 
 	def test_one(self):
 		teacher = Teacher(firstName="Jan", lastName="Kowalski", pesel="86110298656")
@@ -22,7 +22,7 @@ class Test_totalAverage:
 				subject=subject
 			),
 		])
-		assert_that(totalAverage(grades)).is_close_to(2.0, tolerance=0.001)
+		assert_that(calculateTotalAverage(grades)).is_close_to(2.0, tolerance=0.001)
 
 	def test_three(self):
 		teacher = Teacher(firstName="Jan", lastName="Kowalski", pesel="86110298656")
@@ -47,4 +47,4 @@ class Test_totalAverage:
 				subject=subject
 			),
 		])
-		assert_that(totalAverage(grades)).is_close_to(3.0, tolerance=0.001)
+		assert_that(calculateTotalAverage(grades)).is_close_to(3.0, tolerance=0.001)
