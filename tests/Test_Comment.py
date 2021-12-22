7d4f7362f5f514ee46efc27a70d58e5f9043cd32
Rafał Majewski <goodheropl@gmail.com>
2021-12-22 08:42:57 +0100

    add assertpy to the project

--- tests/Test_Comment.py
+++ tests/Test_Comment.py
@@ -1,20 +1,22 @@
 import unittest
 from modules.Comment import Comment
 from datetime import datetime as Datetime
+from assertpy import assert_that  # type: ignore
 
 
 class Test_Comment_constructor(unittest.TestCase):
 	def test_no_arguments(self):
-		self.assertRaises(TypeError, Comment)
+		assert_that(Comment).raises(TypeError).when_called_with()
 
 	def test_if_sets_content(self):
 		datetime = Datetime(year=2020, month=1, day=1)
 		content = "Test content"
 		comment = Comment(datetime=datetime, content=content)
-		self.assertEqual(comment.content, content)
+		assert_that(comment.content).is_equal_to(content)
 
 	def test_wrong_content_type(self):
 		datetime = Datetime(year=2020, month=1, day=1)
 		content = 1
-		with self.assertRaises(TypeError):
-			Comment(datetime=datetime, content=content)
+		assert_that(Comment).raises(TypeError).when_called_with(
+			datetime=datetime, content=content
+		)
