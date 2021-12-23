25cc790b3e77505cf5571ae0422b024079ab6f4b
Rafał Majewski <goodheropl@gmail.com>
2021-12-23 15:46:05 +0100

    parameterize whole Test_deserializeStudent class

--- tests/Test_csvUtils.py
+++ tests/Test_csvUtils.py
@@ -7,7 +7,7 @@ from modules.csvUtils import \
 import unittest
 import unittest.mock
 from modules.Student import Student
-from parameterized import parameterized_class, parameterized  # type: ignore
+from parameterized import parameterized_class  # type: ignore
 from modules.Subject import Subject
 from modules.Teacher import Teacher
 from modules.GradeValue import GradeValue
@@ -45,25 +45,28 @@ class Test_serializeStudent(unittest.TestCase):
 		self.assertEqual(serializeStudent(self.student), self.expectedString)
 
 
+@parameterized_class(
+	("studentString", "expectedStudent"),
+	[
+		(
+			"76072443188;Jan;Kowalski",
+			Student(pesel="76072443188", firstName="Jan", lastName="Kowalski")
+		),
+		(
+			"86110298656;Adam;Nowak",
+			Student(pesel="86110298656", firstName="Adam", lastName="Nowak")
+		),
+		(
+			"05320732334;Anna;Kowalska",
+			Student(pesel="05320732334", firstName="Anna", lastName="Kowalska")
+		),
+	]
+)
 class Test_deserializeStudent(unittest.TestCase):
-	@parameterized.expand(
-		[
-			(
-				"76072443188;Jan;Kowalski",
-				Student(pesel="76072443188", firstName="Jan", lastName="Kowalski")
-			),
-			(
-				"86110298656;Adam;Nowak",
-				Student(pesel="86110298656", firstName="Adam", lastName="Nowak")
-			),
-			(
-				"05320732334;Anna;Kowalska",
-				Student(pesel="05320732334", firstName="Anna", lastName="Kowalska")
-			),
-		]
-	)
-	def test_correct(self, studentString: str, expectedStudent: Student):
-		self.assertEqual(deserializeStudent(studentString), expectedStudent)
+	def test_correct(self):
+		self.assertEqual(
+			deserializeStudent(self.studentString), self.expectedStudent
+		)
 
 
 class Test_exportStudents(unittest.TestCase):
