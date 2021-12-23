c0b8df6d6b68cb61f55ebb060d58906162ebe8ec
Rafał Majewski <goodheropl@gmail.com>
2021-12-23 14:22:31 +0100

    move duplicate code to serializeGrade function

--- modules/csvUtils.py
+++ modules/csvUtils.py
@@ -1,8 +1,18 @@
 from modules.Student import Student
+from modules.Grade import Grade
 
 
 def serializeStudent(student: Student) -> str:
 	return f"{student.pesel};{student.firstName};{student.lastName}"
+
+
+def serializeGrade(grade: Grade) -> str:
+	return (
+		f"{grade.teacher.pesel};"
+		f"{grade.subject.id};"
+		f"{grade.datetime};"
+		f"{grade.value.name}"
+	)
 
 
 def exportStudents(students: set[Student], filepath: str):
@@ -17,22 +27,11 @@ def exportStudentsGrades(students: set[Student], filepath: str):
 		file.write("studentPesel;teacherPesel;subjectId;datetime;value\n")
 		for student in students:
 			for grade in student.grades:
-				file.write(
-					f"{student.pesel};"
-					f"{grade.teacher.pesel};"
-					f"{grade.subject.id};"
-					f"{grade.datetime};"
-					f"{grade.value.name}\n"
-				)
+				file.write(f"{student.pesel};" + serializeGrade(grade) + "\n")
 
 
 def exportStudentGrades(student: Student, filepath: str):
 	with open(filepath, "w") as file:
 		file.write("teacherPesel;subjectId;datetime;value\n")
 		for grade in student.grades:
-			file.write(
-				f"{grade.teacher.pesel};"
-				f"{grade.subject.id};"
-				f"{grade.datetime};"
-				f"{grade.value.name}\n"
-			)
+			file.write(serializeGrade(grade) + "\n")
